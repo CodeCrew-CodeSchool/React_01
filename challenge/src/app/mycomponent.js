33e398a7feb50d43efc69dc5bfaa95f49d2700b8
DJ <dj@code-crew.org>
2025-04-07 10:54:04 -0500

    challenge demo

---
+++ challenge/src/app/mycomponent.js
@@ -0,0 +1,9 @@
+export default function MyComponent() {
+    return(
+        <>
+            <h1>Hello, Class!</h1>
+            <p>Let's write some React!</p>
+            <img src="/bhack24.png" alt="hacker"></img>
+        </>
+    );
+}
